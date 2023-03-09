f966e1efae3198549fcab420c088f4a9a4b3e499
Puranjit Singh Matharoo <puranjit13@gmail.com>
2023-03-09 17:54:53 -0600

    Add files via upload
    Signed-off-by: Puranjit Singh Matharoo <puranjit13@gmail.com>

---
+++ 26 AbstractClass.java
@@ -0,0 +1,37 @@
+// Abstract class
+abstract class Rapid {
+    abstract public void drive();
+}
+
+// Abstract class
+abstract class Octavia extends Rapid {
+    abstract public void tyres();
+}
+
+// Concrete class - in which all the methods are defined using the concept --
+// OverRiding
+class Skoda extends Octavia {
+
+    public void drive() {
+        System.out.println("The drive is very good!!");
+    }
+
+    public void tyres() {
+        System.out.println("The company uses Michelin tyres in new cars!!");
+    }
+
+}
+
+public class AbstractClass {
+
+    public static void main(String[] args) {
+
+        // We always use the concrete class as an object to define the instance in the
+        // main method
+
+        Octavia sk = new Skoda();
+        sk.drive(); // method @overriding in Skoda class
+        sk.tyres(); // method @overriding in Skoda class
+    }
+
+}
